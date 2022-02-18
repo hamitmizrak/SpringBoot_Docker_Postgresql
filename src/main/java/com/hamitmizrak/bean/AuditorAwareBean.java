129f6a7ed9af7b3a2584d07c0fa522a73abe567b
hamitmizrak <hamitmizrak@gmail.com>
2022-02-18 22:35:31 +0300

    AuditorAwareBean

---
+++ src/main/java/com/hamitmizrak/bean/AuditorAwareBean.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak.bean;
+
+import com.hamitmizrak.audit.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+@Configuration
+public class AuditorAwareBean {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+
+        return new AuditorAwareImpl();
+    }
+
+}
