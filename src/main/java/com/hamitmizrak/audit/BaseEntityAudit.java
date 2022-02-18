28a0d8fcb5959d0d290d06ebdb2be8a054365ed3
hamitmizrak <hamitmizrak@gmail.com>
2022-02-18 22:34:50 +0300

    BaseEntityAudit

---
+++ src/main/java/com/hamitmizrak/audit/BaseEntityAudit.java
@@ -0,0 +1,47 @@
+package com.hamitmizrak.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+
+//JSON verisinde gözükmesi istenmeyen anahtarlar belirtilir.
+@JsonIgnoreProperties(value = {"created_date,update_date"},allowGetters = true)
+public class BaseEntityAudit {
+
+    @Id
+    @Column(name = "id", nullable = false,updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //kim ekledi
+    @Column(name = "created_by", nullable = false)
+    @CreatedBy
+    private String createdBy;
+
+    //kim ne zaman oluşturdu
+    @Column(name = "created_date", nullable = false,updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @CreatedDate
+    private Date createdDate;
+
+    //kim güncelledi
+    @Column(name = "update_by", nullable = false)
+    @LastModifiedBy
+    private String updateBy;
+
+    @Column(name = "update_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedDate;
+}
