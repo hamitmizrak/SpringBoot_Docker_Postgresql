385e5c51635be62ad359b35e10587c916eca3caf
hamitmizrak <hamitmizrak@gmail.com>
2022-02-18 22:36:54 +0300

    @SpringBootApplication 1-) Audit aktifleştirme ve bean tanıtmak 2-) Security kapatmak

--- src/main/java/com/hamitmizrak/SpringBootDockerPostgresqlApplication.java
+++ src/main/java/com/hamitmizrak/SpringBootDockerPostgresqlApplication.java
@@ -2,15 +2,18 @@ package com.hamitmizrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //spring security dahil etmemek
-@SpringBootApplication
-(exclude =
+@SpringBootApplication(exclude =
 {
   org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
   org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 }
 )
+
+//Audit aktif etmek icin auditorAware ==> Bean adı
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SpringBootDockerPostgresqlApplication {
 
     public static void main(String[] args) {
