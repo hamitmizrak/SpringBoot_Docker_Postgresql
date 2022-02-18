24dfe6bea77caf26f8e7bf40dddb419050da3d5d
hamitmizrak <hamitmizrak@gmail.com>
2022-02-18 22:36:06 +0300

    ProductEntity ve BaseEntityAware

---
+++ src/main/java/com/hamitmizrak/entity/ProductEntity.java
@@ -0,0 +1,37 @@
+package com.hamitmizrak.entity;
+
+import com.hamitmizrak.audit.BaseEntityAudit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+
+@Entity
+@Table(name = "product")
+public class ProductEntity extends BaseEntityAudit implements Serializable {
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_trade")
+    private String productTrade;
+
+
+
+
+
+
+}
