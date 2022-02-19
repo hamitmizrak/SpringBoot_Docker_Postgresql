b22076ba19d6dcab02cc51186ce4c9df9dd27fc3
hamitmizrak <hamitmizrak@gmail.com>
2022-02-19 12:50:13 +0300

    Docker postgresql tamamdır

--- src/main/java/com/hamitmizrak/SpringBootDockerPostgresqlApplication.java
+++ src/main/java/com/hamitmizrak/SpringBootDockerPostgresqlApplication.java
@@ -1,5 +1,6 @@
 package com.hamitmizrak;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +18,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 //Audit aktif etmek icin auditorAware ==> Bean adı
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@Log4j2
 public class SpringBootDockerPostgresqlApplication  implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -27,20 +29,16 @@ public class SpringBootDockerPostgresqlApplication  implements CommandLineRunner
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+
     @Override
     public void run(String... args) throws Exception {
+        String sql = "INSERT INTO product (product_name, product_trade) VALUES ("
+                + "'Ürün adı', 'bilgisayar')";
 
+        int rows = jdbcTemplate.update(sql);
+        if (rows > 0) {
+            System.out.println("A new row has been inserted.");
+        }
     }
-
-//    @Override
-//    public void run(String... args) throws Exception {
-//        String sql = "INSERT INTO students (name, email) VALUES ("
-//                + "'Nam Ha Minh', 'nam@codejava.net')";
-//
-//        int rows = jdbcTemplate.update(sql);
-//        if (rows > 0) {
-//            System.out.println("A new row has been inserted.");
-//        }
-//    }
 
 }
